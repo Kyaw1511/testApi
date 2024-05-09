633ed059cbaa42b7f2a1a7246244608ed765bdfd
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-09 22:29:06 +0630

    pagination 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
-  <div>
-    <h1>hello world</h1>
-  </div>
 
   <!-- test -->
   <div class="row">
-    <div class="col-md-3">
-      
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-info mx-2">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">API</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
     </div>
+    <div class="col-md-3"></div>
 
     <!-- array of array -->
     <div
-      class="col-md-9">
+      class="col-md-6">
       <ul
         class="list-group">
         <li
           v-for="user in users.data" :key="user"
-          class="list-group-item my-2 border border-dark">
-          <div>
+          class="list-group-item my-2 border border-dark flex">
+          <div class="float-start">
             <h4>
               No.{{ user.id }} 
             </h4>
@@ -27,14 +43,18 @@
             <h4>
               <pre>Last Name  : {{ user.last_name }}</pre>
             </h4>
-            <h3>
+            <h4>
               <pre>Email      : {{ user.email }}</pre>
-            </h3>
-            <img :src="user.avatar" :key="avatar" alt="">
-
+            </h4>
           </div>
           
-          <div>
+          <div class="float-end">
+            <img 
+              :src="user.avatar" :key="avatar" alt=""
+              class="float-end"
+            >
+          </div>
+          <!-- <div class="">
             <h3
               v-for="person in people" :key="person.email">
               {{ user.email }}
@@ -44,12 +64,13 @@
               class="btn btn-sm btn-primary my-1">
               Detail
             </button>
-          </div>
+          </div> -->
 
         </li>
       </ul>
     </div> 
   
+    <div class="col-md-3"></div>
   </div>
   
 </template>
