628a0620e0b51f93bd96a60ec159225474e653b1
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-11 22:55:08 +0630

    pagination 2.2

--- src/App.vue
+++ src/App.vue
@@ -136,6 +136,41 @@
   //   rows.items.length
   // })
 
+  // const perPage = ref(3)
+  // const currentPage = ref([
+  //   items = (rows) => ([
+  //   { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+  //   { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+  //   { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+  //   { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+  //   { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+  //   { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+  //   { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+  //   { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+  //   { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+  // ]),
+  // rows = computed(() => {
+  //   return items.value.length
+  // })
+  // ])
+
+  const perPage = ref(3);
+const currentPage = ref(1);
+const items = [
+  { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+  { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+  { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+  { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+  { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+  { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+  { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+  { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+  { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+];
+
+const rows = computed(() => {
+  items.value
+});
   
   
   // const storeUser = () => {
